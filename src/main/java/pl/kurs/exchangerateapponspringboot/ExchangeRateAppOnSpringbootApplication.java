929f190dc555c2a6480381e6a6ad89292ad1f837
Wiktor Wach <wiktor2604@gmail.com>
2023-07-07 20:19:42 +0200

    initial commit

---
+++ src/main/java/pl/kurs/exchangerateapponspringboot/ExchangeRateAppOnSpringbootApplication.java
@@ -0,0 +1,15 @@
+package pl.kurs.exchangerateapponspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRateAppOnSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeRateAppOnSpringbootApplication.class, args);
+    }
+
+
+    //todo skad springboot bierze wersje do h2
+}
